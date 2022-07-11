09a7e4f933677b8659e50a96501308cb483d705c
projectworlds <33458568+projectworldsofficial@users.noreply.github.com>
2022-07-11 19:08:25 +0530

    new sql file

--- table.sql
+++ table.sql
@@ -1,23 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
--- http://www.phpmyadmin.net
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2014 at 07:29 AM
--- Server version: 5.5.36
--- PHP Version: 5.4.25
+-- Generation Time: Jul 11, 2022 at 03:34 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.39
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `secure_login`
+-- Database: `table`
 --
 
 -- --------------------------------------------------------
@@ -26,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `login_attempts`
 --
 
-CREATE TABLE IF NOT EXISTS `login_attempts` (
+CREATE TABLE `login_attempts` (
   `user_id` int(11) NOT NULL,
   `time` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,7 +43,8 @@ INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
 (2, '1395432481'),
 (2, '1395432607'),
 (3, '1395432637'),
-(2, '1395513130');
+(2, '1395513130'),
+(9, '1657545023');
 
 -- --------------------------------------------------------
 
@@ -49,8 +52,8 @@ INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
 -- Table structure for table `members`
 --
 
-CREATE TABLE IF NOT EXISTS `members` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` char(128) NOT NULL,
@@ -62,15 +65,36 @@ CREATE TABLE IF NOT EXISTS `members` (
   `pect` varchar(220) DEFAULT '0',
   `pic` int(11) DEFAULT '0',
   `picName` mediumtext,
-  `paid` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+  `paid` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `members`
 --
 
+INSERT INTO `members` (`id`, `username`, `email`, `password`, `salt`, `admin`, `days`, `present`, `absent`, `pect`, `pic`, `picName`, `paid`) VALUES
+(10, 'admin', 'admin@admin.com', 'f8b4ffb8ee85ce78e47cbc5deca41f02b0bb53269a5ed8acb0d3d034d8069f01ee3e8d3fe5a627f53bd633ac87ee5db7520751c43ade9b20b57b4311aa5a89ee', '68c56b59e6deba2158e22b1b8fe42ba8f12062bcfd24196cc2f434135666f385ffd26e2a8791a57678b2f7700c1b69b78da4fc474c43d1414b824ffe01890747', 1, '0', '0', '0', '0', 0, NULL, 0);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
